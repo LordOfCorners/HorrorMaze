8c0dff76db2a6ad503de2c1ee92144fb803f2ecb
Michael <kaham340@newschool.edu>
2014-03-07 13:52:08 -0500

    added footsteps

---
+++ HorrorMaze_v3/Scene Unity Project/Assets/footsteps.js
@@ -0,0 +1,16 @@
+﻿var AudioFile : AudioClip;
+
+function Update () {
+	if ( Input.GetKeyDown (KeyCode.W) || Input.GetKeyDown (KeyCode.A) || Input.GetKeyDown (KeyCode.S) || Input.GetKeyDown (KeyCode.D) )
+	{
+	    audio.clip = AudioFile;
+	    audio.Play();
+	 
+	}
+	
+	if ( Input.GetKeyUp (KeyCode.W) || Input.GetKeyUp (KeyCode.A) || Input.GetKeyUp (KeyCode.S) || Input.GetKeyUp (KeyCode.D) )
+	{
+    audio.clip = AudioFile;
+    audio.Stop();
+	}
+}
