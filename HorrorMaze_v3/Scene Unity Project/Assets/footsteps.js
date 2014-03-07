28d561a45e3f7b0d0f125586df9a3e12ba5d4470
Michael <kaham340@newschool.edu>
2014-03-07 13:56:41 -0500

    Added sounds to arrow keys

--- HorrorMaze_v3/Scene Unity Project/Assets/footsteps.js
+++ HorrorMaze_v3/Scene Unity Project/Assets/footsteps.js
@@ -1,16 +1,18 @@
 ﻿var AudioFile : AudioClip;
 
 function Update () {
-	if ( Input.GetKeyDown (KeyCode.W) || Input.GetKeyDown (KeyCode.A) || Input.GetKeyDown (KeyCode.S) || Input.GetKeyDown (KeyCode.D) )
+	if ( Input.GetKeyDown (KeyCode.W) || Input.GetKeyDown (KeyCode.A) || Input.GetKeyDown (KeyCode.S) 
+	|| Input.GetKeyDown (KeyCode.D) || Input.GetKeyDown (KeyCode.UpArrow) || Input.GetKeyDown (KeyCode.LeftArrow) 
+	|| Input.GetKeyDown (KeyCode.RightArrow) || Input.GetKeyDown (KeyCode.DownArrow))
 	{
 	    audio.clip = AudioFile;
 	    audio.Play();
-	 
 	}
 	
-	if ( Input.GetKeyUp (KeyCode.W) || Input.GetKeyUp (KeyCode.A) || Input.GetKeyUp (KeyCode.S) || Input.GetKeyUp (KeyCode.D) )
+	if ( Input.GetKeyUp (KeyCode.W) || Input.GetKeyUp (KeyCode.A) || Input.GetKeyUp (KeyCode.S) || Input.GetKeyUp (KeyCode.D)
+	|| Input.GetKeyUp (KeyCode.UpArrow) || Input.GetKeyUp (KeyCode.DownArrow) || Input.GetKeyUp (KeyCode.LeftArrow) || Input.GetKeyUp (KeyCode.RightArrow))
 	{
-    audio.clip = AudioFile;
-    audio.Stop();
+	    audio.clip = AudioFile;
+	    audio.Stop();
 	}
 }
